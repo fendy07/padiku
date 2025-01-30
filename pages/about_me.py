9d11bc743e62d88ca238c5324642bf656189e90a
fendy07 <hendriyantofendy07@gmail.com>
2025-01-30 10:19:19 +0700

    update about me

--- pages/about_me.py
+++ pages/about_me.py
@@ -6,7 +6,7 @@ with col1:
 with col2:
     st.title("Fendy Hendriyanto", anchor=False)
     st.write(
-        "AI & Data Science Instructor"
+        "AI Researcher & Instructor | ML Engineer"
     )
     st.write(
         "Assisting and mentoring students to help analyze and supporting data driven with creativity and decision making."
@@ -17,8 +17,8 @@ st.write("\n")
 st.subheader("Experience and Qualifications", anchor=False)
 st.write(
     """
-    - 2 years experience coaching and mentoring about Artificial Intelligence
-    - Strong hands-on experience and knowledge in Python and Data Science
+    - 2 years experience coaching and mentoring about Artificial Intelligence and Data Science
+    - Strong hands-on experience and knowledge in Python programming and Data Science
     - Proficient in using various libraries and tools such as TensorFlow, Keras, Scikit-learn, OpenCV, Pandas
     - Good understanding and analyzing of statistical principles and their perspective applications
     - Excellent team player and initiative on tasks
@@ -31,12 +31,13 @@ st.write("\n")
 st.subheader("Hard Skills", anchor=False)
 st.write(
     """
-    - Programming : Python (Pandas, Scikit-learn, Scikit-image), R, SQL, JavaScript
-    - Data Visualization : Tableau, Spreadsheet, Excel
-    - Modelling : Tensorflow, Keras, PyCaret, XGBoost, CometML
+    - Programming Languages : Python, R, SQL, JavaScript
+    - Data Analysis and Visualization : Pandas, Matplotlib, Seaborn, Tableau, Spreadsheet, Excel
+    - Modelling : Tensorflow, Keras, PyCaret, PyTorch, XGBoost, CometML, Scikit-learn
     - Databases : MySQL, PostgreSQL, SQLite
-    - Deployment : Streamlit, Flask, Gradio, Huggingface, Git
-    - Frameworks : OpenCV, NLTK
+    - Deployment : Streamlit, Flask, Gradio, Huggingface
+    - Version Control: Git, GitHub
+    - CV and NLP Frameworks : OpenCV, NLTK, Scikit-image, Pillow
 
     """
 )
